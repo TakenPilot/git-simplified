eae1c43bc747c08110811739687862523e978f33
Dane Stuckel <dane.stuckel@gmail.com>
2014-08-18 12:27:42 -0400

    prevent bad things

--- start-task.sh
+++ start-task.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 git branch | grep "* master"
-if [ $? -eq 0 ] ; then
-	git checkout -b $1
-	git push origin $1
-else
-	echo "Must be master"
+is_master=$?
+
+git branch | grep $1
+exists=$?
+
+if [ $exists -eq 1 ] ; then
+	if [ $is_master -eq 0 ] ; then
+		git checkout -b $1
+		git push origin $1
+	else
+		echo "Must be master"
+	fi
+else 
+    echo "Already exists"
 fi
