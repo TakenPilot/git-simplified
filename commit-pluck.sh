4eebbabcfa662c62c0388712aab235609727f70c
Dane Stuckel <dane.stuckel@gmail.com>
2014-08-18 12:34:39 -0400

    remove specific commit

---
+++ commit-pluck.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    exit
+fi
+
+git branch | grep "* master"
+is_master=$?
+
+git log --pretty=oneline | grep $1
+exists=$?
+
+if [ $exists -eq 0 ] ; then
+	if [ $is_master -eq 1 ] ; then
+		git rebase --onto $1^ $1
+	else
+		echo "Cannot be master"
+	fi
+else 
+    echo "Hash must exist"
+fi
